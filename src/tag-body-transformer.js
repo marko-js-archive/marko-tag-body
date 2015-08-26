4aa60d115e7c43f59d9a3bd88b76d152ebff8c19
Kristian Mandrup <kmandrup@gmail.com>
2015-08-26 17:20:38 +0100

    introduce -or variant

--- src/tag-body-transformer.js
+++ src/tag-body-transformer.js
@@ -1,10 +1,20 @@
 module.exports = function transform(node, compiler, template) {
     var bodyExpression = node.getAttribute('tag-body');
+    var bodyExpressionOr = node.getAttribute('tag-body-or');
+
     if (bodyExpression != null) {
         node.removeAttribute('tag-body');
 
         node.appendChild(compiler.createNode('tag-body', {
             body: bodyExpression || 'data.renderBody'
+        }));
+    }
+
+    if (bodyExpressionOr != null) {
+        node.removeAttribute('tag-body-or');
+
+        node.appendChild(compiler.createNode('tag-body', {
+            body: bodyExpression + ' || data.renderBody'
         }));
     }
 };
